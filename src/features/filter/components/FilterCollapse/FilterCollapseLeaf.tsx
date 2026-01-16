cc3fcc0b0f2ce7848f81864f3baf9bd86e754208
daeseopk <eotjqdlee@gmail.com>
2026-01-16 15:58:02 +0900

    feat: update FilterCollapseLeaf component

--- src/features/filter/components/FilterCollapse/FilterCollapseLeaf.tsx
+++ src/features/filter/components/FilterCollapse/FilterCollapseLeaf.tsx
@@ -1,5 +1,6 @@
 import type { CollapseAPI, CollapseItem } from "@/components/ui/collapse";
 import type { FilterItem } from "@/features/filter/components/FilterCollapse/FilterCollapse";
+import { Trash } from "lucide-react";
 import { useMemo } from "react";
 
 interface FilterCollapseLeafProps {
@@ -19,5 +20,24 @@ export default function FilterCollapseLeaf(props: FilterCollapseLeafProps) {
     };
   }, [item]);
 
-  return <div>leaf</div>;
+  return (
+    <div className="flex flex-row items-center justify-between w-full">
+      <div className="flex flex-row items-center gap-2">
+        <span className="text-sm font-medium text-foreground">
+          {leafItem.label}
+        </span>
+        {leafItem.extra && (
+          <span className="text-sm text-muted-foreground">
+            {leafItem.extra}
+          </span>
+        )}
+      </div>
+      <Trash
+        width={16}
+        height={16}
+        className="cursor-pointer justify-self-end"
+        onClick={(e) => e.stopPropagation()}
+      />
+    </div>
+  );
 }
