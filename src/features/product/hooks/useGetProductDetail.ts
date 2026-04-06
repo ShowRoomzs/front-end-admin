89df086850783038a9ac2a7d389562b6ddd9d42b
daeseopk <eotjqdlee@gmail.com>
2026-04-06 16:31:06 +0900

    refactor: simplify product detail query key in useGetProductDetail hook

--- src/features/product/hooks/useGetProductDetail.ts
+++ src/features/product/hooks/useGetProductDetail.ts
@@ -1,14 +1,11 @@
-import { PRODUCT_LIST_QUERY_KEY } from "@/features/product/constants/queryKey";
-import {
-  productService,
-} from "@/features/product/services/productService";
+import { productService } from "@/features/product/services/productService";
 import { useQuery } from "@tanstack/react-query";
 
 const PRODUCT_DETAIL_QUERY_KEY = "product-detail";
 
 export function useGetProductDetail(productId: number) {
   return useQuery({
-    queryKey: [PRODUCT_LIST_QUERY_KEY, PRODUCT_DETAIL_QUERY_KEY, productId],
+    queryKey: [PRODUCT_DETAIL_QUERY_KEY, productId],
     queryFn: () => productService.getProductDetail(productId),
     enabled: !!productId,
   });
